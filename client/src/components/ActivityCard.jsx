b59c1ee7c714bb997ee258af36bd46cf10be1cce
nishamvp <107843151+nishamvp@users.noreply.github.com>
2024-07-11 17:44:38 +0400

    "Refactor ActivityCard component: updated styles and structure"

--- client/src/components/ActivityCard.jsx
+++ client/src/components/ActivityCard.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 const ActivityCard = () => {
   return (
-    <div className="flex flex-col items-center bg-primary font-dm-sans font-bold gap-4 max-w-96">
-      <img className="" src="activity.jpg" alt="" />
-      <h2 className=" text-tertiary text-xl">Drawing Contest for kids</h2>
-      <button className="bg-tertiary text-primary w-full py-2">
-        READ MORE
-      </button>
+    <div className="font-dm-sans font-bold  max-w-96">
+      <div className="bg-primary flex flex-col items-center gap-4">
+        <div className="overflow-hidden w-full">
+          <img
+            className="transform transition-transform duration-500 hover:scale-110 w-full object-cover "
+            src="activity.jpg"
+            alt="Activity"
+          />
+        </div>
+        <h2 className="text-tertiary text-xl hover:underline">Drawing Contest for Kids</h2>
+        <button className="bg-tertiary text-primary w-full py-2 mt-2  hover:bg-quarternary transition-colors duration-300">
+          READ MORE
+        </button>
+      </div>
     </div>
   )
 }
